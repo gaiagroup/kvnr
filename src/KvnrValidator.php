275df9c119221ac6ef2e16f5dc03a6d4c9563f70
davidedanna-GAIA <davide.danna@gaia-group.com>
2020-09-08 16:21:06 +0200

    PASY-255 Second code optimization

--- src/KvnrValidator.php
+++ src/KvnrValidator.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * @author    Davide Danna
+ * @copyright 2020 GAIA AG, Hamburg
+ * @package   KVNR-Validator
+ *
+ * Created using PhpStorm at 07.09.20 10:36
+ */
+
 namespace GaiaGroup;
 
 /**
@@ -15,13 +23,14 @@ class KvnrValidator
      *
      * @return bool true / false
      */
-    public static function validate( string $kvnr ) : bool {
+    public static function validate(string $kvnr): bool
+    {
         $isValid = false;
 
         // checks KVNR format using regex
-        if( preg_match( "/^[A-Z][0-9]{9}$/", $kvnr) ) {
+        if (preg_match("/^[A-Z][0-9]{9}$/", $kvnr)) {
             // computes CRC digit using KVNR string trimmed out of the last digit
-            $kvnrCrc = self::computesKvnrCrcDigit( substr($kvnr,0,strlen($kvnr)-1) );
+            $kvnrCrc = self::computesKvnrCrcDigit(substr($kvnr, 0, strlen($kvnr) - 1));
 
             // compares computed CRC values with last digit in provided KVNR
             $isValid = $kvnr[9] == $kvnrCrc;
@@ -38,50 +47,47 @@ class KvnrValidator
      *
      * @return int CRC digit, if code is invalid returns -1
      */
-    private static function computesKvnrCrcDigit(string $kvnr ) {
+    private static function computesKvnrCrcDigit(string $kvnr)
+    {
         $crc = -1;
-        $kvnrDigits = [];
 
         // converts first character of KVNR to integer using ASCII
-        $digitLetter = ord($kvnr[0]) - 64;
+        $digitChar = ord($kvnr[0]) - 64;
 
         // checks if conversion gave expected values (A->1 ... Z->26)
-        if( $digitLetter <= 26 && $digitLetter >= 1 ) {
+        if ($digitChar <= 26 && $digitChar >= 1) {
+            $kvnrDigits = [];
+
             // adds 0 left-padding for values less than 10
-            $digitLetter = str_pad( strval($digitLetter), 2, "0", STR_PAD_LEFT);
+            $digitChar = str_pad(strval($digitChar), 2, "0", STR_PAD_LEFT);
 
             // sets first 2 element of digits array
-            $kvnrDigits[] = intval( $digitLetter[0] );
-            $kvnrDigits[] = intval( $digitLetter[1] );
-        }
+            $kvnrDigits[] = intval($digitChar[0]);
+            $kvnrDigits[] = intval($digitChar[1]);
 
-        if( !empty( $kvnrDigits ) ) {
             // sets last 8 element of digits array
-            for( $i=2; $i<10; $i++ ) {
-                $kvnrDigits[$i] = intval( $kvnr[$i-1] );
+            for ($i = 2; $i < 10; $i++) {
+                $kvnrDigits[$i] = intval($kvnr[$i - 1]);
             }
 
-            if( count($kvnrDigits) == 10 ) {
-                $kvnrDigitsWeighted = [];
+            $kvnrDigitsWeighted = [];
+            for ($i = 0; $i < 10; $i++) {
+                // sets weight array: (1, 2, 1, 2, ...)
+                $weight = ($i % 2 == 0) ? 1 : 2;
 
-                for( $i=0; $i<10; $i++ ) {
-                    // sets weight array: (1, 2, 1, 2, ...)
-                    $weight = ($i % 2 == 0) ? 1 : 2;
+                // multiplies digit array with weight array
+                $kvnrDigitWeighted = $kvnrDigits[$i] * $weight;
 
-                    // multiplies digit array with weight array
-                    $kvnrDigitWeighted = $kvnrDigits[$i] * $weight;
-
-                    // if resulting number is >= 10 then it's digit are summed
-                    if( $kvnrDigitWeighted < 10 ) {
-                        $kvnrDigitsWeighted[$i] = $kvnrDigitWeighted;
-                    } else {
-                        $kvnrDigitsWeighted[$i] = array_sum(str_split($kvnrDigitWeighted));
-                    }
+                // if resulting number is >= 10 then it's digit are summed
+                if ($kvnrDigitWeighted < 10) {
+                    $kvnrDigitsWeighted[$i] = $kvnrDigitWeighted;
+                } else {
+                    $kvnrDigitsWeighted[$i] = array_sum(str_split($kvnrDigitWeighted));
                 }
-
-                // computes CRC by summing each item in array and applying module-10
-                $crc = array_sum( $kvnrDigitsWeighted ) % 10;
             }
+
+            // computes CRC by summing each item in array and applying module-10
+            $crc = array_sum($kvnrDigitsWeighted) % 10;
         }
 
         return $crc;
